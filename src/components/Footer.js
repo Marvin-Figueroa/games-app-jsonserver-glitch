2b4c8afb129fed3f286cb927829c341ca29fa5e5
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-18 13:32:18 -0600

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer class="footer">
+      <small class="footer__copyright">
+        Created by
+        <span class="footer__author"> Marvin Figueroa</span> &copy; 2022 -
+        Powered by
+        <span class="footer__powered"> React JS</span>
+      </small>
+    </footer>
+  );
+};
+
+export default Footer;
