99beb6c8369d92a8428f27fcdd50a0bf77571ba1
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-18 16:01:53 -0600

    Change class attribute to className to the elements inside the Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
 const Footer = () => {
   return (
-    <footer class="footer">
-      <small class="footer__copyright">
+    <footer className="footer">
+      <small className="footer__copyright">
         Created by
-        <span class="footer__author"> Marvin Figueroa</span> &copy; 2022 -
+        <span className="footer__author"> Marvin Figueroa</span> &copy; 2022 -
         Powered by
-        <span class="footer__powered"> React JS</span>
+        <span className="footer__powered"> React JS</span>
       </small>
     </footer>
   );
