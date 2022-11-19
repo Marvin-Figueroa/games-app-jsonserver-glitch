d1f146cd5e14ee176dd0f822c19dcb7df2eaa15e
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-19 13:20:10 -0600

    Add click event handler to the game's title

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,12 +1,20 @@
-const GameCard = ({ name, rating, image }) => {
+import "./GameCard.scss";
+
+const GameCard = ({ id, name, rating, image, handleGameSelect }) => {
   return (
-    <aricle className="game-card">
-      <img src={image} alt="" className="game-card__image" />
-      <div className="game-card__text">
-        <h2 className="game-card__title">{name}</h2>
-        <p className="game-card__rating">{rating}</p>
+    <article className="game-card">
+      <div className="game-card__image-container">
+        <img src={image} alt="" className="game-card__image" />
       </div>
-    </aricle>
+      <div className="game-card__text">
+        <h2 className="game-card__title">
+          <a onClick={() => handleGameSelect(id)} href="#">
+            {name}
+          </a>
+        </h2>
+        <p className="game-card__rating">{rating} / 5.0</p>
+      </div>
+    </article>
   );
 };
 
