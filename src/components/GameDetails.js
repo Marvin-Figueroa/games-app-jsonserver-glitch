a1f6418c89dec284b45302d9557946ce934e0e6b
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-19 13:18:04 -0600

    Add logic to get comments by game from json-server

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -1,16 +1,33 @@
+import { useEffect, useState } from "react";
 import CommentsList from "./CommentsList";
+import HttpClient from "../services/http";
+const http = new HttpClient("https://plume-gelatinous-asp.glitch.me");
 
-const GameDetails = ({ name, image, released, rating, description }) => {
+const GameDetails = ({ game }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments();
+  }, []);
+
+  async function getComments() {
+    // setLoading(true);
+    const gameComments = await http.get(`/games/${game.id}/comments`);
+    console.log(comments);
+    setComments(gameComments);
+    // setLoading(false);
+  }
+
   return (
     <article className="game">
-      <img src={image} alt="" className="game__image" />
+      <img src={game.image} alt="" className="game__image" />
       <div className="game__info">
-        <h2 className="game__title">{name}</h2>
-        <span className="game_date">{released}</span>
-        <span className="game__rating">{rating}</span>
-        <p className="game__description">{description}</p>
+        <h2 className="game__title">{game.name}</h2>
+        <span className="game_date">{game.released}</span>
+        <span className="game__rating">{game.rating}</span>
+        <p className="game__description">{game.description}</p>
       </div>
-      <CommentsList />
+      <CommentsList comments={comments} />
     </article>
   );
 };
