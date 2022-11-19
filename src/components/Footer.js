5751713dc0097a492e5000c332db5e0c1cb63629
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-19 13:12:58 -0600

    Remove unnecessary markup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,11 @@
+import "./Footer.scss";
+
 const Footer = () => {
   return (
     <footer className="footer">
       <small className="footer__copyright">
         Created by
-        <span className="footer__author"> Marvin Figueroa</span> &copy; 2022 -
-        Powered by
-        <span className="footer__powered"> React JS</span>
+        <span className="footer__author"> Marvin Figueroa</span> &copy; 2022
       </small>
     </footer>
   );
