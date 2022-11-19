88960c88f74cf8d890a9193ad5cfff12357120d1
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-19 13:26:04 -0600

    Pass the necessary props to each GameCard component

--- src/components/GameCardsGrid.js
+++ src/components/GameCardsGrid.js
@@ -1,10 +1,18 @@
 import GameCard from "./GameCard";
+import "./GameCardsGrid.scss";
 
-const GameCardsGrid = ({ games }) => {
+const GameCardsGrid = ({ games, handleGameSelect }) => {
   return (
     <section className="games-container">
       {games.map((game) => (
-        <GameCard />
+        <GameCard
+          key={game.id}
+          id={game.id}
+          name={game.name}
+          rating={game.rating}
+          image={game.image}
+          handleGameSelect={handleGameSelect}
+        />
       ))}
     </section>
   );
