670d523708d04cbdd4257503ffa25264ba2c7c4b
Marvin Figueroa <marvinfigueroa2012@gmail.com>
2022-11-21 17:58:04 -0600

    Add logic to render the Pagination component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,48 @@ import React, { useEffect, useState } from "react";
 import HttpClient from "./services/http";
 
 import "./App.scss";
+
 import GameCardsGrid from "./components/GameCardsGrid";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./components/About";
+import Pagination from "./components/Pagination";
 
 import HashLoader from "react-spinners/HashLoader";
 import GameDetails from "./components/GameDetails";
+
 const http = new HttpClient("https://plume-gelatinous-asp.glitch.me");
 
 function App() {
   const [games, setGames] = useState([]);
+  const [gamesCount, setGamesCount] = useState(0);
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState("games");
   const [selectedGame, setSelectedGame] = useState(null);
+  const [currGamesPage, setCurrGamesPage] = useState(1);
 
   useEffect(() => {
     getGames();
   }, []);
 
+  useEffect(() => {
+    getPaginatedGames(currGamesPage);
+  }, [currGamesPage]);
+
   async function getGames() {
     setLoading(true);
-    const gamesList = await http.get("/games");
+    const res = await http.get("/games?_page=1&_limit=2", {
+      parseResponse: false,
+    });
+
+    setGamesCount(Number(res.headers.get("x-total-count")));
+    const gamesList = await res.json();
 
     setGames(gamesList);
     setLoading(false);
   }
 
   function handleClick(currentPage) {
-    console.log(currentPage);
     setCurrentPage(currentPage);
   }
 
@@ -39,7 +52,15 @@ function App() {
     const game = games.find((game) => Number(game.id) === Number(id));
     setSelectedGame(game);
     setCurrentPage("game");
-    console.log(game);
+  }
+
+  async function handlePageChange(page) {
+    setCurrGamesPage(page);
+  }
+
+  async function getPaginatedGames(page) {
+    const newGames = await http.get(`/games?_page=${page}&_limit=8`);
+    setGames(newGames);
   }
 
   return (
@@ -59,6 +80,13 @@ function App() {
               (currentPage === "about" && <About />) ||
               (currentPage === "game" && <GameDetails game={selectedGame} />)}
           </main>
+          {currentPage === "games" && (
+            <Pagination
+              itemsCount={gamesCount}
+              pageSize={8}
+              onPageChange={handlePageChange}
+            />
+          )}
           <Footer />
         </>
       )}
